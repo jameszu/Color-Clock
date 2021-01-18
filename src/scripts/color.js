b7587ffaf3ab5185d4fbed9307ae12db2800810b
James ~ <zqxhjjh@outlook.com>
2021-01-18 22:03:45 +1300

    disable picker panel

--- src/scripts/color.js
+++ src/scripts/color.js
@@ -73,7 +73,7 @@ let red, green, blue;
 function showTime()
 {
     date = new Date();
-    hour = date.getHours();
+    hour    = date.getHours();
     minute = date.getMinutes();
     second = date.getSeconds();
     // console.log(hour, minute, second);
@@ -105,44 +105,45 @@ function showTime()
 }
 
 
-function initColorPicker() {
-    var canvas = document.getElementById('colorCanvas');
-    var canvasContext = canvas.getContext('2d');
+// function initColorPicker() 
+// {
+//     var canvas = document.getElementById('colorCanvas');
+//     var canvasContext = canvas.getContext('2d');
   
-    let gradient = canvas.getContext('2d').createLinearGradient(0, 0, canvas.width, 0)
-    gradient.addColorStop(0, '#ff0000')
-    gradient.addColorStop(1 / 6, '#ffff00')
-    gradient.addColorStop((1 / 6) * 2, '#00ff00')
-    gradient.addColorStop((1 / 6) * 3, '#00ffff')
-    gradient.addColorStop((1 / 6) * 4, '#0000ff')
-    gradient.addColorStop((1 / 6) * 5, '#ff00ff')
-    gradient.addColorStop(1, '#ff0000')
-    canvas.getContext('2d').fillStyle = gradient
-    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+//     let gradient = canvas.getContext('2d').createLinearGradient(0, 0, canvas.width, 0)
+//     gradient.addColorStop(0, '#ff0000')
+//     gradient.addColorStop(1 / 6, '#ffff00')
+//     gradient.addColorStop((1 / 6) * 2, '#00ff00')
+//     gradient.addColorStop((1 / 6) * 3, '#00ffff')
+//     gradient.addColorStop((1 / 6) * 4, '#0000ff')
+//     gradient.addColorStop((1 / 6) * 5, '#ff00ff')
+//     gradient.addColorStop(1, '#ff0000')
+//     canvas.getContext('2d').fillStyle = gradient
+//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
   
-    gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
-    gradient.addColorStop(0, 'rgba(255, 255, 255, 1)')
-    gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)')
-    gradient.addColorStop(1, 'rgba(255, 255, 255, 0)')
-    canvas.getContext('2d').fillStyle = gradient
-    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+//     gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
+//     gradient.addColorStop(0, 'rgba(255, 255, 255, 1)')
+//     gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)')
+//     gradient.addColorStop(1, 'rgba(255, 255, 255, 0)')
+//     canvas.getContext('2d').fillStyle = gradient
+//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
   
-    gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
-    gradient.addColorStop(0, 'rgba(0, 0, 0, 0)')
-    gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)')
-    gradient.addColorStop(1, 'rgba(0, 0, 0, 1)')
-    canvas.getContext('2d').fillStyle = gradient
-    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+//     gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
+//     gradient.addColorStop(0, 'rgba(0, 0, 0, 0)')
+//     gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)')
+//     gradient.addColorStop(1, 'rgba(0, 0, 0, 1)')
+//     canvas.getContext('2d').fillStyle = gradient
+//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
   
   
-    canvas.onclick = function(e) {
-        console.log()
-      var imgData = canvasContext.getImageData((e.offsetX / canvas.clientWidth) * canvas.width, (e.offsetY / canvas.clientHeight) * canvas.height, 1, 1)
-      var rgba = imgData.data;
-      var color = "rgba(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ", " + rgba[3] + ")";
-      console.log("%c" + color, "color:" + color)
-    }
-  }
+//     canvas.onclick = function(e) {
+//         console.log()
+//       var imgData = canvasContext.getImageData((e.offsetX / canvas.clientWidth) * canvas.width, (e.offsetY / canvas.clientHeight) * canvas.height, 1, 1)
+//       var rgba = imgData.data;
+//       var color = "rgba(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ", " + rgba[3] + ")";
+//       console.log("%c" + color, "color:" + color)
+//     }
+// }
   
-  initColorPicker()
+// initColorPicker()
 showTime();
