2e57d62275631965c3a48f7babbb5a2701693258
jameszu <zqxhjjh@outlook.com>
2021-01-26 17:35:53 +1300

    wait

--- src/scripts/color.js
+++ src/scripts/color.js
@@ -1,13 +1,13 @@
-
-function componentToHex(c) 
+// Main display
+function addZero(c) 
 {
-    let hex = c.toString();
-    return hex.length == 1 ? "0" + hex : hex;
+    let num = c.toString();
+    return num.length == 1 ? "0" + num : num;
 }
 
 function timeToHex(h, m, s)
 {
-    return "#" + componentToHex(h) + componentToHex(m) + componentToHex(s);
+    return "#" + addZero(h) + addZero(m) + addZero(s);
 }
 
 function HexToRgb(hex)
@@ -18,7 +18,7 @@ function HexToRgb(hex)
     let g = (bigint >> 8) & 255;
     let b = bigint & 255;
 
-    return "rgb(" + r + ","+ g + "," + b + ")";
+    return "rgb(" + addZero(r) + ","+ addZero(g) + "," + addZero(b) + ")";
 }
 
 function RgbToHsl(r, g, b)
@@ -65,15 +65,43 @@ function RgbToHsl(r, g, b)
     s = +(s * 100).toFixed(1);
     l = +(l * 100).toFixed(1);
 
-    return "hsl(" + h + "," + s + "%," + l + "%)";
+    return "hsl(" + addZero(h) + "," + addZero(s) + "%," + addZero(l) + "%)";
 }
+
+function rgbToCmyk(r, g, b)
+{
+    let c, m , y, k = 0;
+
+    if (r == 0 && g == 0 && b == 0)
+    {
+        k = 1;
+        return "cmyk(" + addZero(c) + "," + addZero(m) + "," + addZero(y) + "," + addZero(k) + ")";
+    }
+    c = 1 - (r / 255);
+    m = 1 - (g / 255);
+    y = 1 - (b / 255);
+
+    let mincmy = (Math.min(c, Math.min(m, y)));
+
+    c = (c - mincmy) / (1 - mincmy);
+    m = (m - mincmy) / (1 - mincmy);
+    y = (y - mincmy) / (1 - mincmy);
+    k = mincmy;
+    c = +(c * 100).toFixed(0);
+    m = +(m * 100).toFixed(0);
+    y = +(y * 100).toFixed(0);
+    k = +(k * 100).toFixed();
+    return "cmyk(" + addZero(c) + "," + addZero(m) + "," + addZero(y) + "," + addZero(k) + ")";
+}
+
+
 let date, hour, minute, second, colorTime;
 let red, green, blue;
 
 function showTime()
 {
     date = new Date();
-    hour    = date.getHours();
+    hour = date.getHours();
     minute = date.getMinutes();
     second = date.getSeconds();
     // console.log(hour, minute, second);
@@ -90,169 +118,37 @@ function showTime()
 
     hslTime = RgbToHsl(red, green, blue);
 
-    if (hour<=9) hour = '0' + hour;
-    if (minute<=9) minute = '0' + minute;
-    if (second<=9) second = '0' + second;
-    colorTime = hour + ":" + minute + ":" + second;
+    colorTime = addZero(hour) + ":" + addZero(minute) + ":" + addZero(second);
 
+    cmykTime = rgbToCmyk(red, green, blue);
 
     document.getElementById("time").innerHTML = colorTime;
     document.getElementById("hexcolor").innerHTML = hexcolor;
     document.getElementById("rgbcolor").innerHTML = rgbcolor;
     document.getElementById("hslcolor").innerHTML = hslTime;
-    setTimeout(showTime, 500);
+    document.getElementById("cmykcolor").innerHTML = cmykTime;
+
+    const slide = document.getElementById("slider");
+
+    const ele = document.getElementById("changeColor");
+
+    if (ele.checked)
+    {
+        slide.style.background = hexcolor;
+    }
+    else
+    {
+        slide.style.background = "red";
+
+    }
+
+    setTimeout(showTime, 500);  
 
 }
 
 
-// function initColorPicker() 
-// {
-//     var canvas = document.getElementById('colorCanvas');
-//     var canvasContext = canvas.getContext('2d');
-  
-//     let gradient = canvas.getContext('2d').createLinearGradient(0, 0, canvas.width, 0)
-//     gradient.addColorStop(0, '#ff0000')
-//     gradient.addColorStop(1 / 6, '#ffff00')
-//     gradient.addColorStop((1 / 6) * 2, '#00ff00')
-//     gradient.addColorStop((1 / 6) * 3, '#00ffff')
-//     gradient.addColorStop((1 / 6) * 4, '#0000ff')
-//     gradient.addColorStop((1 / 6) * 5, '#ff00ff')
-//     gradient.addColorStop(1, '#ff0000')
-//     canvas.getContext('2d').fillStyle = gradient
-//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
-  
-//     gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
-//     gradient.addColorStop(0, 'rgba(255, 255, 255, 1)')
-//     gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)')
-//     gradient.addColorStop(1, 'rgba(255, 255, 255, 0)')
-//     canvas.getContext('2d').fillStyle = gradient
-//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
-  
-//     gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
-//     gradient.addColorStop(0, 'rgba(0, 0, 0, 0)')
-//     gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)')
-//     gradient.addColorStop(1, 'rgba(0, 0, 0, 1)')
-//     canvas.getContext('2d').fillStyle = gradient
-//     canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
-  
-  
-//     canvas.onclick = function(e) {
-//         console.log()
-//       var imgData = canvasContext.getImageData((e.offsetX / canvas.clientWidth) * canvas.width, (e.offsetY / canvas.clientHeight) * canvas.height, 1, 1)
-//       var rgba = imgData.data;
-//       var color = "rgba(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ", " + rgba[3] + ")";
-//       console.log("%c" + color, "color:" + color)
-//     }
-// }
-  
-// initColorPicker()
-
-class Picker {
-    constructor(target, width, height) {
-      this.target = target;
-      this.width = width;
-      this.height = height;
-      this.target.width = width;
-      this.target.height = height;
-      //Get context 
-      this.context = this.target.getContext("2d");
-      //Circle 
-      this.pickerCircle = { x: 10, y: 10, width: 7, height: 7 };
-      
-      this.listenForEvents();
-    }
-    
-    draw() {
-      this.build();
-    }
-    
-    build() {
-      let gradient = this.context.createLinearGradient(0, 0, this.width, 0);
-  
-      //Color Stops
-      gradient.addColorStop(0, "rgb(255, 0, 0)");
-      gradient.addColorStop(0.15, "rgb(255, 0, 255)");
-      gradient.addColorStop(0.33, "rgb(0, 0, 255)");
-      gradient.addColorStop(0.49, "rgb(0, 255, 255)");
-      gradient.addColorStop(0.67, "rgb(0, 255, 0)");
-      gradient.addColorStop(0.84, "rgb(255, 255, 0)");
-      gradient.addColorStop(1, "rgb(255, 0, 0)");
-      //Fill it
-      this.context.fillStyle = gradient;
-      this.context.fillRect(0, 0, this.width, this.height);
-      
-      //Apply black and white 
-          gradient = this.context.createLinearGradient(0, 0, 0,          this.height);
-      gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-      gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
-      gradient.addColorStop(0.5, "rgba(0, 0, 0, 0)");
-      gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
-      this.context.fillStyle = gradient;
-      this.context.fillRect(0, 0, this.width, this.height);
-      
-      //Circle 
-      this.context.beginPath();
-      this.context.arc(this.pickerCircle.x, this.pickerCircle.y, this.pickerCircle.width, 0, Math.PI * 2);
-      this.context.strokeStyle = "black";
-      this.context.stroke();
-      this.context.closePath();
-      
-    }
-    
-    listenForEvents() {
-      let isMouseDown = false;
-      const onMouseDown = (e) => {
-        let currentX = e.clientX - this.target.offsetLeft;
-        let currentY = e.clientY - this.target.offsetTop;
-        if(currentY > this.pickerCircle.y && currentY < this.pickerCircle.y + this.pickerCircle.width && currentX > this.pickerCircle.x && currentX < this.pickerCircle.x + this.pickerCircle.width) {
-          isMouseDown = true;
-        } else {
-          this.pickerCircle.x = currentX;
-          this.pickerCircle.y = currentY;
-        }
-      }
-      
-      const onMouseMove = (e) => {
-        if(isMouseDown) {
-         let currentX = e.clientX - this.target.offsetLeft;
-         let currentY = e.clientY - this.target.offsetTop;
-          this.pickerCircle.x = currentX;
-          this.pickerCircle.y = currentY;
-        }
-      }
-      
-      const onMouseUp = () => {
-        isMouseDown = false;
-      }
-      
-      //Register 
-      this.target.addEventListener("mousedown", onMouseDown);
-      this.target.addEventListener("mousemove", onMouseMove);
-      this.target.addEventListener("mousemove", () => this.onChangeCallback(this.getPickedColor()));
-  
-      
-      document.addEventListener("mouseup", onMouseUp);
-    }
-    
-    getPickedColor() {
-      let imageData = this.context.getImageData(this.pickerCircle.x, this.pickerCircle.y, 1, 1);
-      return { r: imageData.data[0], g: imageData.data[1], b: imageData.data[2] };
-    }
-    
-    onChange(callback) {
-      this.onChangeCallback = callback;
-    }
-    
-    
-  }
-  
-let picker = new Picker(document.getElementById("color-picker"), 250, 220);
-
-//Draw 
-setInterval(() => picker.draw(), 1);
-
-picker.onChange((color) => {
-    let selected = document.getElementsByClassName("selected")[0];
-selected.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-});
 showTime();
+
+
+// Toggle Button setting
+
